df07457cff3d8d418f6731e9fa49ed5fb36a6ad9
Kazi Abu Sayem Md. Nayem <66078436+nayem15-12925@users.noreply.github.com>
2020-06-24 22:25:57 +0600

    Create LCM.cpp

---
+++ LCM.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+       return b; 
+    if (b == 0) 
+       return a; 
+    if (a == b) 
+        return a; 
+   
+    if (a > b) 
+        return gcd(a-b, b); 
+
+    return gcd(a, b-a); 
+} 
+
+int main() 
+{ 
+    int a, b; 
+    cout<< "Enter two integer number = ";
+    cin >> a >> b;
+    cout<<"LCM is "<< (a*b)/gcd(a, b) << endl; 
+    return 0; 
+} 
